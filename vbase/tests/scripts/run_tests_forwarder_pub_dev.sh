ba16aaa01428b75657403fbcf0c8a1d216d13d7c
greg-vbase <greg@vbase.com>
2024-10-29 14:35:13 -0700

    Added forwarder public dev service tests

---
+++ vbase/tests/scripts/run_tests_forwarder_pub_dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -a
+source config/.env.forwarder.pub.dev
+set +a
+
+python3 -m unittest vbase.tests.test_vbase_client
+python3 -m unittest vbase.tests.test_indexing_service
+python3 -m unittest vbase.tests.test_vbase_dataset
