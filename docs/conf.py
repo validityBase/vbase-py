c3e46873765020f282007f8b9c51334faeafc2b4
greg-vbase <greg+github@vbase.com>
2025-03-08 10:52:40 -0800

    Ruff initial commit

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,8 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
 
 import os
 import sys
@@ -12,8 +13,9 @@ sys.path.insert(0, os.path.abspath(".."))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "vbase-py"
-# Drop the trailing period since Sphinx adds it.
-copyright = "2023-2024, PIT Labs, Inc."
+# Disable A001 Variable is shadowing a Python builtin
+# since the variable is needed for Sphinx build.
+copyright = "2023-2024, PIT Labs, Inc."  # noqa: A001
 author = "PIT Labs, Inc."
 
 # -- General configuration ---------------------------------------------------
